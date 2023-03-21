37a118def118ce5cde5a2859b8fc70cbac04cfb4
Arsenii <knight.fo@yandex.ru>
2023-03-21 20:03:06 +0000

    now working for sure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import "./App.css"
 import Join from './pages/join';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
 import About from './pages/about';
 
 
 function App() {
   return (
-  <BrowserRouter>
+    <div>
     <div className='navbar'>
       <div className='navbar__links'>
-        <a href='/hot-beans-web/join'>Join</a>
-        <a href='/hot-beans-web/about'>About</a>
+        <NavLink to='/hot-beans-web/join'>Join</NavLink>
+        <NavLink to='/hot-beans-web/about'>About</NavLink>
       </div>
     </div>
 
 
     <Route path="/hot-beans-web/about" component={About} /> 
     <Route path="/hot-beans-web/join" component={Join} />
-    </BrowserRouter>
+  </div>
   );
 };
   
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </HashRouter>
 );
